d1108e0c8ab3d0a997abdb38c74778a6f43a46d8
ben1214 <aaron690322@icloud.com>
2023-07-27 21:43:21 +0800

    modify

--- password.py
+++ password.py
@@ -1,12 +1,14 @@
 password = 'a123456'
 i = 3
-while True:
-    x = input('請輸入密碼')
+while i > 0:
+    i = i - 1
+    x = input('請輸入密碼: ')
     if x == password:
         print('登入成功')
         break 
     else:
-        i = i - 1
-        print('密碼錯誤 還有', i ,' 次機會')
-        if i == 0: 
-            break 
+        print('密碼錯誤')
+        if i > 0:
+            print('還有',i,'次機會')
+        else:
+            print('lock')
