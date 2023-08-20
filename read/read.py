947dd7a383cc2e5e3858b714a1be123645750695
ben1214 <aaron690322@icloud.com>
2023-08-20 16:48:07 +0800

    文字計數

--- read/read.py
+++ read/read.py
@@ -8,6 +8,8 @@ with open('reviews.txt','r') as f:
 			print(len(data))
 print('檔案讀取完了,總共有', len(data),'筆資料')
 
+
+
 sum_len = 0
 for d in data:
 	sum_len = sum_len + len(d)
@@ -23,3 +25,54 @@ for d in data:
 print('一共有',len(new),'比長度小於100')
 print(new[0])
 print(new[1])
+
+
+good = []
+for d in data:
+	if 'good' in d:
+		good.append(d)
+print('一共有', len(good), '筆留言提到good')
+
+
+#文字計數
+
+wc = {} # word_count
+for d in data:
+	words = d.split(' ')
+	for word in words:
+		if word in wc:
+			wc[word] += 1
+		else:
+			wc[word] = 1 # 新增new key in wc dictionary
+for word in wc:
+	if wc[word] > 1000000:
+		print(word, wc[word])
+
+print(len(wc))
+print(wc['alice'])
+
+while True:
+	word = input('查啥?')
+	if word == 'exit':
+		break
+	if word in wc:
+		print(word, '出現', wc[word], '次')
+	else:
+		print('沒出現過')
+
+print('tks using')
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
